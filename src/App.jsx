6e7b3d8e1b7c733f196626950c9c9a410cc205b7
Ali <alihajjhussein63@gmail.com>
2025-04-15 15:04:34 +0300

    added: Welcome Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import "./index.css";
+
+import WelcomePage from "./pages/WelcomePage";
+import { AnimatePresence } from 'framer-motion';
+
+const LandingPage = ({ showWelcome, setShowWelcome }) => {
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomePage onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      {!showWelcome && (
+        <>
+          <h1 className="text-center text-white mt-10">Mwah 😘</h1>
+        </>
+      )}
+    </>
+  );
+};
+
+
 
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
   return (
-    <h1 className="text-red-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="myportfolio/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
